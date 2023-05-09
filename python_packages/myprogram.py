e9c608378d10ecc1fdcb09d9ef000287eb620a3f
Bikramjit Singh <singhbikramjit222@gmail.com>
2023-05-08 19:43:07 -0700

    Error handling and Various test cases

---
+++ python_packages/myprogram.py
@@ -0,0 +1,10 @@
+# from mymodule import my_func
+
+# my_func()
+
+from MyMainPackage import some_man_script
+from MyMainPackage.Subpackage import mysubscript
+
+some_man_script.report_main()
+
+mysubscript.sub_report()
